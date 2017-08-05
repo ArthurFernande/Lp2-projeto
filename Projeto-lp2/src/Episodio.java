52b9ef00617c3dbd5778ac1c46b932178da69a5e
enzoraian <enzo.candido@ccc.ufcg.edu.br>
2017-08-05 13:54:15 -0300

    Add files via upload

---
+++ Projeto-lp2/src/Episodio.java
@@ -0,0 +1,29 @@
+package projeto;
+
+/**
+ * Projeto Lp2 - Tracking Things
+ * @author Enzo Raian
+ *
+ */
+
+public class Episodio {
+	private int NumEpisodio;
+	private float duracao;
+	
+	public void Episodio(){
+		this.NumEpisodio = NumEpisodio;
+		this.duracao = duracao;
+	}
+
+	public int getNumEpisodio() {
+		return this.NumEpisodio;
+	}
+
+	public float getDuracao() {
+		return this.duracao;
+	}
+	
+	public String toString(){
+		return this.NumEpisodio + " " + this.duracao;
+	}
+}
