60fc189be54476961eaf9fe26cbb984040ed476b
enzoraian <enzo.candido@ccc.ufcg.edu.br>
2017-08-05 13:44:14 -0300

    Add files via upload

---
+++ Projeto-lp2/src/Serie.java
@@ -0,0 +1,39 @@
+package projeto;
+
+/**
+ * Projeto lp2 - Tracking Things
+ * @author Enzo Raian
+ */
+public class Serie {
+	private String genero;
+	private int temporada;
+	// falta hashset
+	
+	public Serie(String genero, int temporada){
+		this.genero = genero;
+		this.temporada = temporada;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+
+	public int getTemporada() {
+		return temporada;
+	}
+
+	public void setTemporada(int temporada) {
+		this.temporada = temporada;
+	}
+
+	public String toString(){
+		return this.genero + " " + this.temporada;
+	}
+	// Falta getepisodios
+	// Falta equals
+	
+}
