ea5947d893a33fa4fe914690952ab18628f40987
enzoraian <enzo.candido@ccc.ufcg.edu.br>
2017-08-06 17:26:22 -0300

    Update Emprestimo.java

--- Projeto-lp2/src/Emprestimo.java
+++ Projeto-lp2/src/Emprestimo.java
@@ -1,7 +1,9 @@
+package projeto;
+import java.util.ArrayList;
+
 /**
  * Projeto Lp2 - Tracking Things
  * @author Enzo Raian
- *
  */
 public class Emprestimo {
 	private String donoDoItem;
@@ -10,6 +12,7 @@ public class Emprestimo {
 	private String inicioDoEmprestimo;
 	private int diasCombinados;
 	private String dataDevolucao;
+	private ArrayList <Object> ListaDeItens = new ArrayList(); 
 	
 	public Emprestimo(String donoDoItem, String tomadorDoEmprestimo, String item, String inicioDoEmprestimo,
 			int diasCombinados, String dataDevolucao) {
@@ -21,17 +24,32 @@ public class Emprestimo {
 		this.dataDevolucao = dataDevolucao;
 	}
 	
-	public void Emprestimo(String donoDoItem, String tomadorDoEmprestimo, String Item, String inicioDoEmprestimo){
-	// implementar metodo	
+	/**
+	 * @param donoDoItem indica o nome do dono do item.
+	 * @param tomadorDoEmprestimo o nome do usuário que pegou o item.
+	 * @param Item indica o item emprestado.
+	 * @param inicioDoEmprestimo indica a data que o item foi emprestado.
+	 * @return Emprestimo Efetuado ou Não Efetuado.
+	 */
+	public String Emprestimo(String donoDoItem, String tomadorDoEmprestimo, String Item, String inicioDoEmprestimo){
+		Emprestimo novoEmprestimo = new Emprestimo(donoDoItem,tomadorDoEmprestimo,Item,inicioDoEmprestimo,diasCombinados,dataDevolucao);
+		if (ListaDeItens.contains(novoEmprestimo)){
+			return "O Item indicado já está emprestado";
+		}
+		else{
+			ListaDeItens.add(novoEmprestimo);
+			return "Emprestimo efetuado com sucesso!";
+		}
 	}
 	
 	/**
-	 * 
-	 * @param diasAtraso 
-	 * @param dataDeEntrega 
+	 * @param diasAtraso indica a quantidade de dias que o item está atrasado. 
+	 * @param dataDeEntrega indica a data real de entrega do item.
+	 * @return
 	 */
-	public void devolverItem(String diasAtraso, String dataDeEntrega){
-	// Falta implementar esse metodo	
+	public String devolverItem(String diasAtraso, String dataDeEntrega){
+		
+		return "Item devolvido com sucesso!";
 	}
 	
 	public String getDataDevolucao() {
