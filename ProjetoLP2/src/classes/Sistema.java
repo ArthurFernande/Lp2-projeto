3d3314c1ce5956c72097c165435773fa302740d3
enzoraian <enzo.candido@ccc.ufcg.edu.br>
2017-08-09 12:01:54 -0300

    Update Sistema.java

--- ProjetoLP2/src/classes/Sistema.java
+++ ProjetoLP2/src/classes/Sistema.java
@@ -1,20 +1,28 @@
 package classes;
 
 import java.util.HashMap;
+import java.util.ArrayList;
 
 public class Sistema {
 	private HashMap<String, Usuario> ListaDeUsuarios = new HashMap();
-
-	public void addUsuario(String nome, String email, String celular){
-		Usuario novoUs = new Usuario(nome,email,celular);
+	private ArrayList<Item> Itens = new ArrayList();
+	
+	public void adicionarUsuario(String nome,String telefone,String email){
+		Usuario novoUs = new Usuario(nome,telefone,email);
 		ListaDeUsuarios.put(novoUs.getNome(),novoUs);
 	}
 	
-	public void removerUsuario(String nome,String celular){
+	public void removerUsuario(String nome,String telefone){
 		if (ListaDeUsuarios.containsKey(nome)){
 			ListaDeUsuarios.remove(nome);
 		}
 	}
 	
-	
+	public void atualizarUsuario(String nome, String telefone){
+		
+	}
+
+	public HashMap<String, Usuario> getListaDeUsuarios() {
+		return ListaDeUsuarios;
+	}
 }
