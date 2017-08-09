ddf9e9f423d3a803cd0053eaa1b5df7b8c520056
ArthurFernande <arthur.andrade@ccc.ufcg.edu.br>
2017-08-09 15:52:16 -0300

    Update Sistema.java

--- ProjetoLP2/src/classes/Sistema.java
+++ ProjetoLP2/src/classes/Sistema.java
@@ -4,25 +4,58 @@ import java.util.HashMap;
 import java.util.ArrayList;
 
 public class Sistema {
-	private HashMap<String, Usuario> ListaDeUsuarios = new HashMap();
+	private HashMap<Chave, Usuario> mapaDeUsuarios = new HashMap();
 	private ArrayList<Item> Itens = new ArrayList();
 	
-	public void adicionarUsuario(String nome,String telefone,String email){
-		Usuario novoUs = new Usuario(nome,telefone,email);
-		ListaDeUsuarios.put(novoUs.getNome(),novoUs);
+	public void cadastrarUsuario(String nome,String telefone,String email){
+		Chave chave = new Chave(nome, telefone);
+		Usuario usuario = new Usuario(nome,telefone,email);
+		mapaDeUsuarios.put(chave,usuario);
 	}
 	
 	public void removerUsuario(String nome,String telefone){
-		if (ListaDeUsuarios.containsKey(nome)){
-			ListaDeUsuarios.remove(nome);
+		Chave chave = new Chave(nome, telefone);
+		if (mapaDeUsuarios.containsKey(chave)){
+			mapaDeUsuarios.remove(chave);
 		}
 	}
 	
-	public void atualizarUsuario(String nome, String telefone){
-		
-	}
-
-	public HashMap<String, Usuario> getListaDeUsuarios() {
-		return ListaDeUsuarios;
+	public void atualizarUsuario(String nome, String telefone, String atributo, String valor){
+		Chave chave = new Chave(nome,telefone);
+		mapaDeUsuarios.remove(chave);
+		switch(atributo){
+			case "Telefone":
+				mapaDeUsuarios.get(chave).setTelefone(valor);
+				Usuario usuarioTemporario = mapaDeUsuarios.get(chave);
+				mapaDeUsuarios.remove(chave);
+				chave.setTelefone(valor);
+				mapaDeUsuarios.put(chave, usuarioTemporario);
+				break;
+			case "Email":
+				mapaDeUsuarios.get(chave).setEmail(valor);
+				break;
+			case "Nome":
+				mapaDeUsuarios.get(chave).setNome(valor);
+				Usuario usuarioTemporario1 = mapaDeUsuarios.get(chave);
+				mapaDeUsuarios.remove(chave);
+				chave.setNome(valor);
+				mapaDeUsuarios.put(chave,usuarioTemporario1);
+				break;
+			default:
+				throw new IllegalArgumentException ("Atributo invalido");
+			}
+		}
+	public String getInfoUsuario(String nome, String telefone, String atributo){
+		Chave chave = new Chave(nome,telefone);
+		switch(atributo){
+		case "Telefone":
+			return mapaDeUsuarios.get(chave).getTelefone();
+		case "Email":
+			return mapaDeUsuarios.get(chave).getEmail();
+		case "Nome":
+			return mapaDeUsuarios.get(chave).getNome();
+		default:
+			throw new IllegalArgumentException ("Atributo invalido");
+		}
 	}
 }
